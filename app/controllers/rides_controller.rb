81a535d156e475c63a09ad92b880493a6100615f
Mehmet <mehmetkusken@gmail.com>
2021-07-12 01:03:58 -0600

    commit tree f40c55a1f0e276b6c1ff12a1fef55fc57d051794

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,16 @@
+class RidesController < ApplicationController
+
+
+    def create
+      ride = Ride.create(ride_params)
+      notice = ride.take_ride
+      flash[:notice] = notice
+      redirect_to user_path(ride.user)
+    end
+  
+   private
+  
+   def ride_params
+     params.require(:ride).permit(:user_id, :attraction_id)
+   end
+  end
